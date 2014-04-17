e650a96df620f1e5003ce437b1c8522c5dd28b40
darshshah <darsh.shah11@gmail.com>
2014-04-17 19:51:20 -0400

    URL rerouting

--- hello.py
+++ hello.py
@@ -1,11 +1,32 @@
 
+
 from flask import Flask
 app = Flask (__name__)
 
 @app.route('/hello')
 def hello_world():
-    return 'Hello World'
+    return 'Hello World!!!'
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return 'User %s' % username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+        return 'Post %d' % post_id
+
+@app.route('/')
+def index():
+    return 'Index'
+
+@app.route('/projects/')
+def projects():
+    return 'The project page'
+
+@app.route('/about')
+def about():
+    return 'The about page'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
 
